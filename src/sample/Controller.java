86d3326176c9ab648fa7e477e397ed0c8984fe84
carlojose <carlo.laitano@acad.pucrs.br>
2018-07-01 01:37:20 -0300

    com botao iniciar apontando para a primeira sala

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,23 @@
 package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class Controller {
     @FXML
-    public void pressButtonStart(ActionEvent event){
-        System.out.println("tal");
+    public void pressButtonStart(ActionEvent event) throws IOException {
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("salaPrisao.fxml"));
+        primaryStage.setTitle("Manicomio");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();;
     }
     @FXML
     public void pressButtonExit(ActionEvent event){
