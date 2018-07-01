5a8f3258a987e8aa11179f3a1935ea8e91ee6e28
carlojose <carlo.laitano@acad.pucrs.br>
2018-07-01 01:29:54 -0300

    com janelas de alert

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,20 @@
 package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 
 
 public class Controller {
     @FXML
     public void pressButtonStart(ActionEvent event){
         System.out.println("tal");
+    }
+    @FXML
+    public void pressButtonExit(ActionEvent event){
+        Alert saida = new Alert(Alert.AlertType.INFORMATION);
+        saida.setTitle("Fim de jogo");
+        saida.setContentText("Até logo");
+        saida.show();
     }
 
 }
